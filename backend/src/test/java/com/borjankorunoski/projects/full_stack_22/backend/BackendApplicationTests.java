0776e9e50f768095d4187135fe3850b429ead32c
borjankorunoski <korunoskiborjan@gmail.com>
2022-09-09 22:12:52 +0200

    Initial commit

---
+++ backend/src/test/java/com/borjankorunoski/projects/full_stack_22/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borjankorunoski.projects.full_stack_22.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
